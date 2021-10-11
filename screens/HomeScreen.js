5e276aa4639e3615db3fe4ca64f0902ec2890c58
Vivek Mishra <v.mishra@f5.com>
2021-10-12 00:00:47 +0530

    Google places api integration

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,13 @@ import React from "react";
 import { SafeAreaView, StyleSheet, Image, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -12,6 +17,36 @@ const HomeScreen = () => {
           source={{
             uri: "https://links.papareact.com/gzs",
           }}
+        />
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          placeholder="Where From?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          minLength={2}
+          enablePoweredByContainer={false}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          debounce={400}
         />
         <NavOptions />
       </View>
